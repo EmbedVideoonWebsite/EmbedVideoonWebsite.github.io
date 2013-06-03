4714d5afe1beda60376b430d74f62f2ce15b3a3e
EmbedVideoonWebsite <EmbedVideoonWebsite@gmail.com>
2013-06-03 15:49:17 +0400

    Embed

---
+++ data/images/engine/js/xframe.js
@@ -0,0 +1,8 @@
+
+/*!
+ * Copyright (c) 2010 0rt3z
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) 
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ */ 
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('2.6={9:d(){b(!2.f)2.f=(\'m\'+l.n()).q(\'.\',\'p\');8 2.f},7:d(){b(e.h)8{3:(1.4.c?1.4.c:e.o),5:e.h};j b(1.4&&1.4.g)8{3:1.4.c,5:1.4.g};j b(1.a)8{3:1.a.c,5:1.a.g};8{3:0,5:0}},B:d(i){$(\'a\').A(\'\').r(\'<k C="\'+i+\'" D="\'+2.6.9()+\'" 9="\'+2.6.9()+\'" E="0" y="0" x="0"></k>\');t 7=2.6.7();$(\'#\'+2.6.9()).s({u:0,v:0,3:7.3,5:7.5,w:\'z\'})}};',41,41,'|document|window|width|documentElement|height|xframe|size|return|id|body|if|clientWidth|function|self|xframe_id_|clientHeight|innerHeight|url|else|iframe|Math|iframe_|random|innerWidth|_|replace|append|css|var|top|left|position|vspace|hspace|absolute|html|show|src|name|frameborder'.split('|'),0,{}));
